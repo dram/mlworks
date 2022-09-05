8229dd51699a33eb215cc31146bfef79378fb421
Xin Wang <dram.wang@gmail.com>
2022-09-05 21:36:10 +0800

    [rts] Remove conflicting declarations

--- src/rts/src/OS/Linux/syscalls.h
+++ src/rts/src/OS/Linux/syscalls.h
@@ -56,35 +56,6 @@ extern char *realpath(__const char *__path,
 
 extern int getpagesize (void);
 
-/* signals */
-
-/* struct sigcontext from /usr/include/asm/signal.h, #ifdeffed __KERNEL__ */
-
-struct sigcontext {
-	unsigned short gs, __gsh;
-	unsigned short fs, __fsh;
-	unsigned short es, __esh;
-	unsigned short ds, __dsh;
-	unsigned long edi;
-	unsigned long esi;
-	unsigned long ebp;
-	unsigned long esp;
-	unsigned long ebx;
-	unsigned long edx;
-	unsigned long ecx;
-	unsigned long eax;
-	unsigned long trapno;
-	unsigned long err;
-	unsigned long eip;
-	unsigned short cs, __csh;
-	unsigned long eflags;
-	unsigned long esp_at_signal;
-	unsigned short ss, __ssh;
-	unsigned long i387;
-	unsigned long oldmask;
-	unsigned long cr2;
-};
-
 /* processes */
 
 extern int getpid(void);
@@ -108,10 +79,6 @@ extern void endpwent(void);
 /* math */
 
 extern int isnan(double);
-
-/* misc */
-
-extern char *sys_errlist [];
 
 /* Return the current machine's Internet number.  */
 extern long int gethostid __P ((void));
